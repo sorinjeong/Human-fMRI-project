8781f42fdf1c3d3cbaac6c2f7328973bbeb91fd9
sorinjeong <127107005+sorinjeong@users.noreply.github.com>
2023-03-09 16:56:18 +0900

    Update get_time_event_from_log.m

--- get_time_event_from_log.m
+++ get_time_event_from_log.m
@@ -1,11 +1,11 @@
 function [time,event] = get_time_event_from_log(LogStr)
 
-brackets = find(LogStr=='[' | LogStr==']');
-col_before_event = find(LogStr==':',1,'last');
-LogChar = char(LogStr);
+% brackets = find(LogStr=='[' | LogStr==']');
+% col_before_event = find(LogStr==':',1,'last');
+% LogChar = char(LogStr);
 
 time = str2double(extractBetween(LogStr,"[","]"));
 if ~isempty(time),time=time(1); end
-event = LogChar(10:end);
+event = LogStr(10:end);
 
 end
