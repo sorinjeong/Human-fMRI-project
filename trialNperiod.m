4dff1a1753867c25db354111b6cb0ababf1c5148
sorinjeong <127107005+sorinjeong@users.noreply.github.com>
2023-03-13 18:50:16 +0900

    Update trialNperiod.m

--- trialNperiod.m
+++ trialNperiod.m
@@ -1,10 +1,30 @@
 
 Str=struct('Time',[],'Trial',[],'Period',[],'Event',[]);
 
-for c=1:length(S.sOCPR)
+var_start = ["scont", "sOCPR"];
+var_end = ["endcont", "eOCPR"];
 
-    startTime = S.sOCPR(c,1);
-    endTime = S.eOCPR(c,1);
+prompt = "OCPR? control? or both? ";
+x = input(prompt)
+if x == "OCPR"
+    var_start = ["sOCPR"];
+    var_end = ["eOCPR"];
+elseif x == "control"
+    var_start = ["scont"];
+    var_end = ["endcont"];
+elseif x == "both" | isempty(prompt)
+    var_start = ["scont", "sOCPR"];
+    var_end = ["endcont", "eOCPR"];
+else
+    error('Input must be one of the three above')
+end
+
+for c=1:length(S.scont)
+    for e=1:length(var_start)
+        A=S.(var_start{e}); B=S.(var_end{e});
+
+    startTime = A(c,1);
+    endTime = B(c,1);
     timeBetween_p = startTime <= S.period(1:end,1) & S.period(1:end,1) <= endTime;
     timeBetween_d = startTime <= S.decision(1:end,1) & S.decision(1:end,1) <= endTime;
     [tp_r, tp_c] = find(timeBetween_p);
@@ -12,7 +32,7 @@ for c=1:length(S.sOCPR)
 
 %% start
     S_Time = startTime;
-    S_Trial = S.sOCPR(c,2);
+    S_Trial = A(c,2);
     S_Period = "1";
     S_Event = "start";
 
@@ -91,6 +111,7 @@ end
     Str.Period= [Str.Period; E_Period];
     Str.Event= [Str.Event; E_Event];
 
+    end
 end
 
 %% make a table
