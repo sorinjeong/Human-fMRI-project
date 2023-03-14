e6d0875d5e7f86e04d545a0a14b40bc0cb7e5672
sorinjeong <127107005+sorinjeong@users.noreply.github.com>
2023-03-14 12:53:27 +0900

    Update trialNperiod.m

--- trialNperiod.m
+++ trialNperiod.m
@@ -1,7 +1,7 @@
-FileList = {'CL121121_1','CL121122_1','CL121128_1','CL121227_1','CL130107_1','CL130109_1','CL130114_2','CL130116_2',...
-    'CL130121_2','CL130122_1','CL130130_1','CL130219_1','CL130220_1','CL130225_2','CL130226_1','CL130227_1'};
+% FileList = {'CL121121_1','CL121122_1','CL121128_1','CL121227_1','CL130107_1','CL130109_1','CL130114_2','CL130116_2',...
+%     'CL130121_2','CL130122_1','CL130130_1','CL130219_1','CL130220_1','CL130225_2','CL130226_1','CL130227_1'};
 
-% FileList ={'CL130122_1'};
+FileList ={'CL130122_1'};
 
 %%
 for fi = 1:numel(FileList)
@@ -25,11 +25,11 @@ for fi = 1:numel(FileList)
 EventName = ["Pause", "causeevent timeframe","timespent", "Timeframe", ...
     "relocation", "enter_arm", "enter_gate_", "exit_gate_", "causeevent scanend",...
     "task number", "start_control", "end_control", "correcttrials", "answer", ...	
-    "choice", "start_OCPR", "end_OCPR", "decision", "period"];
+    "choice", "start_OCPR", "end_OCPR", "causeevent decision", "decision", "period"];
 
 VarName = ["Pause", "CTF", "timespent", "Timeframe", "reloc", "arm",...
     "entgate", "exgate", "CSC", "TN", "scont", "endcont", "correct",...
-    "answer", "choice", "sOCPR", "eOCPR", "decision", "period"];
+    "answer", "choice", "sOCPR", "eOCPR", "caus_decision", "decision", "period"];
 
     %% make time and event table using function
     addpath 'C:\Users\sorin\Documents\MATLAB\23.03.06_Log error arrange'
@@ -66,9 +66,9 @@ for i = 1:height(timeNevent)
          digit = [];
          digit = extract(timeNevent.event(i),digitsPattern);
        if ~isempty(digit)
-           S.(VarName{j})= [S.(VarName{j}); timeNevent.time(i), extractBefore(timeNevent.event(i), EventName{j})+extractAfter(timeNevent.event(i), EventName{j})];   
+           S.(VarName{j})= [S.(VarName{j}); timeNevent.time(i), extractBefore(timeNevent.event(i), EventName{j})+extractAfter(timeNevent.event(i), EventName{j}), i];   
        else
-           S.(VarName{j})= [S.(VarName{j}); timeNevent.time(i), ""];
+           S.(VarName{j})= [S.(VarName{j}); timeNevent.time(i), "", i];
        end
 
        break;
@@ -80,14 +80,14 @@ for i = 1:height(timeNevent)
    end
 
    if ~contain_TF
-    Others = [Others; timeNevent.time(i), timeNevent.event(i)];
+    Others = [Others; timeNevent.time(i), timeNevent.event(i),i];
     S.Others = Others; 
    end
 
   end 
 end 
 
-S.eOCPR = [S.eOCPR; timeNevent.time(end), timeNevent.event(end)];
+S.eOCPR = [S.eOCPR; timeNevent.time(end), timeNevent.event(end),i];
 save(['C:\Users\sorin\Documents\MATLAB\23.03.06_Log error arrange\processed\' filenameo], "S")
 
 
@@ -112,8 +112,10 @@ for c=1:length(S.scont)
     endTime = B(c,1);
     timeBetween_p = startTime <= S.period(1:end,1) & S.period(1:end,1) <= endTime;
     timeBetween_d = startTime <= S.decision(1:end,1) & S.decision(1:end,1) <= endTime;
+    timeBetween_cd = startTime <= S.caus_decision(1:end,1) & S.caus_decision(1:end,1) <= endTime;
     [tp_r, tp_c] = find(timeBetween_p);
     [td_r, td_c] = find(timeBetween_d);
+    [tcd_r, tcd_c] = find(timeBetween_cd);
 
 %% start
     S_Time = startTime;
@@ -137,69 +139,125 @@ else
 end
 
 %% period
-    P_Time = S.period(tp_r,1);
+    P_Time = S.period(tp_r,1)';
     P_Trial = S_Trial;
-    P_Period = extract(S.period(tp_r,2), digitsPattern);
+    P_Period = extract(S.period(tp_r,2), digitsPattern)';
     P_Event = "start";
+    P_Log = str2double(S.period(tp_r,3)');
 
-%% dicision
+%% decision
 % if ~isempty(td_r)    
-    D_Time = unique(S.decision(td_r,1));
+    D_Time = unique(S.decision(td_r,1))';
     D_Trial = S_Trial;
     D_Period = P_Period;
     D_Event = "decision";
+    D_Log = str2double(S.decision(td_r,3)');
 
-%% dicision vs period
-if ~isempty(D_Time)
+%% causeevent decision
 
-for i=1:length(P_Time)
-    for j=1:length(D_Time)
-        if P_Time(i) > D_Time(j)
-            Str.Time= [Str.Time; D_Time(j)];
-%             D_Time(j) = NaN;
-            Str.Trial = [Str.Trial; Str.Trial(end)];
-%                         S.decision(td_r,3) = D_Trial;
-            Str.Period = [Str.Period; Str.Period(end)];
-%                         S.decision(td_r,4) = D_Period;
-            Str.Event = [Str.Event; D_Event];
-        elseif P_Time(i) < D_Time(j);
-            break;
-        end
-    end
-    Str.Time = [Str.Time; P_Time(i)];
-            Str.Trial = [Str.Trial; P_Trial];
-            Str.Period = [Str.Period; P_Period(i)];
-            Str.Event = [Str.Event; P_Event];
-end
+    CD_Time = unique(S.caus_decision(tcd_r,1))';
+    CD_Trial = S_Trial;
+    CD_Period = P_Period;
+    CD_Event = "caus_decision";
+    CD_Log = str2double(S.caus_decision(tcd_r,3)');
 
-if length(P_Time) < length(D_Time)
-    for a=length(P_Time)+1:length(D_Time)
-        Str.Time=[Str.Time; D_Time(a)];
-            Str.Trial = [Str.Trial; D_Trial];
-%                         S.decision(td_r,3) = D_Trial;
-            Str.Period = [Str.Period; D_Period(i)];
-%                         S.decision(td_r,4) = D_Period;
-            Str.Event = [Str.Event; D_Event];
-    end
-elseif length(P_Time) == length(D_Time)
-    Str.Time=[Str.Time; D_Time(length(D_Time))];
-            Str.Trial = [Str.Trial; D_Trial];
-%                          S.decision(td_r,3) = D_Trial;
-            Str.Period = [Str.Period; D_Period(length(D_Time))];
-%                          S.decision(td_r,4) = D_Period;
-            Str.Event = [Str.Event; D_Event];
-end
-end
-    %% end
+%% end
     E_Time = endTime;
     E_Trial = S_Trial;
-    E_Period = Str.Period(end);
+%   E_Period = Str.Period(end);
     E_Event = "end";
+    E_Log = str2double(S.endcont(c,3)');
 
-    Str.Time= [Str.Time; E_Time];
-    Str.Trial= [Str.Trial; E_Trial];
-    Str.Period= [Str.Period; E_Period];
-    Str.Event= [Str.Event; E_Event];
+%% new strategy decision vs period
+
+log_order = sort([P_Log D_Log CD_Log E_Log]);
+
+for i=1:length(log_order)
+
+    P_index = find(log_order(i) == P_Log);
+    D_index = find(log_order(i) == D_Log);
+    CD_index = find(log_order(i) == CD_Log);
+    E_index = find(log_order(i) == E_Log);
+
+    if ~isempty(P_index)
+        Str.Time = [Str.Time; P_Time(P_index)];
+        Str.Trial = [Str.Trial; P_Trial];
+        Str.Period = [Str.Period; P_Period(P_index)];
+        Str.Event = [Str.Event; P_Event];
+
+    elseif ~isempty(D_index)
+        Str.Time= [Str.Time; D_Time(D_index)];
+        Str.Trial = [Str.Trial; Str.Trial(end)];
+        Str.Period = [Str.Period; Str.Period(end)];
+        Str.Event = [Str.Event; D_Event];
+
+    elseif ~isempty(CD_index)
+        Str.Time= [Str.Time; CD_Time(CD_index)];
+        Str.Trial = [Str.Trial; Str.Trial(end)];
+        Str.Period = [Str.Period; Str.Period(end)];
+        Str.Event = [Str.Event; CD_Event];
+
+    elseif ~isempty(E_index)
+        Str.Time= [Str.Time; E_Time];
+        Str.Trial= [Str.Trial; E_Trial];
+        Str.Period= [Str.Period; Str.Period(end)];
+        Str.Event= [Str.Event; E_Event];
+    end 
+end
+
+
+% %% decision vs period
+% if ~isempty(D_Time)
+% 
+%     
+% for i=1:length(P_Time)
+%     for j=1:length(D_Time)
+%         if P_Time(i) > D_Time(j)
+%             Str.Time= [Str.Time; D_Time(j)];
+% %             D_Time(j) = NaN;
+%             Str.Trial = [Str.Trial; Str.Trial(end)];
+% %                         S.decision(td_r,3) = D_Trial;
+%             Str.Period = [Str.Period; Str.Period(end)];
+% %                         S.decision(td_r,4) = D_Period;
+%             Str.Event = [Str.Event; D_Event];
+%         elseif P_Time(i) < D_Time(j);
+%             break;
+%         end
+%     end
+%     Str.Time = [Str.Time; P_Time(i)];
+%             Str.Trial = [Str.Trial; P_Trial];
+%             Str.Period = [Str.Period; P_Period(i)];
+%             Str.Event = [Str.Event; P_Event];
+% end
+% 
+% if length(P_Time) < length(D_Time)
+%     for a=length(P_Time)+1:length(D_Time)
+%         Str.Time=[Str.Time; D_Time(a)];
+%             Str.Trial = [Str.Trial; D_Trial];
+% %                         S.decision(td_r,3) = D_Trial;
+%             Str.Period = [Str.Period; D_Period(i)];
+% %                         S.decision(td_r,4) = D_Period;
+%             Str.Event = [Str.Event; D_Event];
+%     end
+% elseif length(P_Time) == length(D_Time)
+%     Str.Time=[Str.Time; D_Time(length(D_Time))];
+%             Str.Trial = [Str.Trial; D_Trial];
+% %                          S.decision(td_r,3) = D_Trial;
+%             Str.Period = [Str.Period; D_Period(length(D_Time))];
+% %                          S.decision(td_r,4) = D_Period;
+%             Str.Event = [Str.Event; D_Event];
+% end
+% end
+%     %% end
+%     E_Time = endTime;
+%     E_Trial = S_Trial;
+%     E_Period = Str.Period(end);
+%     E_Event = "end";
+% 
+%     Str.Time= [Str.Time; E_Time];
+%     Str.Trial= [Str.Trial; E_Trial];
+%     Str.Period= [Str.Period; E_Period];
+%     Str.Event= [Str.Event; E_Event];
 
     end  % both
 end
