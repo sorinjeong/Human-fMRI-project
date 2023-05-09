d20ffa4eb50854f80f143a5fc44196260b1f1910
sorinjeong <127107005+sorinjeong@users.noreply.github.com>
2023-05-09 18:22:09 +0900

    Update OCATLogSampleParsingCode.m

--- OCATLogSampleParsingCode.m
+++ OCATLogSampleParsingCode.m
@@ -11,63 +11,87 @@ for fi = 1:numel(FileList)
     fileToRead1='2023.05.04-18.02.56.230Time_Behavior.csv';
     TimeBehavior = readcell(fileToRead1);
 
-%% EventNameVariable
-EventName = ["LapStart", "TrialStart", "TrialType", "ObjOn","ChoiceOn","Decision", "Duration", "ObjOff", "TrialEnd"];
-
-%% struct wo/coordinate
-EventLog = [];
+%% eventlogfile wo/coordinate
+RawEventLog = [];
 for i=1:height(TimeBehavior)
     if TimeBehavior{i,1}=="X"
         continue
 
     elseif TimeBehavior{i,1}=="LapStart"
-        EventLog = [EventLog; NaN,TimeBehavior(i,1), TimeBehavior(i,4)];
+        RawEventLog = [RawEventLog; NaN,TimeBehavior(i,1), TimeBehavior(i,4)];
 
     elseif TimeBehavior{i,3}=="Duration"
-        EventLog = [EventLog; NaN, TimeBehavior(i,1),TimeBehavior(i,2); NaN, TimeBehavior(i,3), TimeBehavior(i,4)];
+        RawEventLog = [RawEventLog; NaN, TimeBehavior(i,1),TimeBehavior(i,2); NaN, TimeBehavior(i,3), TimeBehavior(i,4)];
 
     elseif TimeBehavior{i,1}=="TrialType"
-        EventLog = [EventLog; NaN,TimeBehavior(i,1), TimeBehavior(i,2)];
+        RawEventLog = [RawEventLog; NaN,TimeBehavior(i,1), TimeBehavior(i,2)];
 
     else
-        EventLog = [EventLog; TimeBehavior(i,4),TimeBehavior(i,1),TimeBehavior(i,3)];
+        RawEventLog = [RawEventLog; TimeBehavior(i,4),TimeBehavior(i,1),TimeBehavior(i,3)];
+    end
+end
+
+%% EventParsing
+
+EventName = ["LapStart", "TrialStart", "TrialType", "ObjOn","ChoiceOn","Decision", "Duration", "ObjOff", "TrialEnd"];
+VarName = ["Lap", "TrialStart", "Trial", "Context", "Direction", "Location","Association", "Obj_ID", "ObjOn","ChoiceOn", "Choice","Decision", "Duration", "ObjOff", "TrialEnd"];
+
+Str = struct;
+for i=1:height(RawEventLog)
+    for j=1:length(EventName)
+        for k=1:length(VarName)
+              if ~isfield(Str,VarName{k})
+                  Str.(VarName{k}) = [];
+              end 
+       % Lap
+       if RawEventLog{i,2} == EventName(1)
+           Str.Lap = [Str.Lap; RawEventLog{i,3}];
+       elseif isempty(Str.Lap) == 0
+           Str.Lap = [Str.Lap; Str.Lap(end)];
+       end
+       % Trial
+       if RawEventLog{i,2} == EventName(2)
+           Str.TrialStart = [Str.TrialStart; RawEventLog{i,1}];
+           if isempty(Str.Trial)
+               Str.Trial = 1;
+           else
+               Str.Trial = [Str.Trial; Str.Trial(end)+1];
+           end
+           if Str.Trial(end) >= 5
+               Str.Trial(end) = 1;
+           end
+       % TrialType
+       elseif RawEventLog{i,2} == EventName(3)
+           Numb = [];
+           Numb = num2str(cell2mat(RawEventLog(i,3))) - '0';
+           if Numb(1) == 1
+               Str.Context = [Str.Context; 'F'];
+           elseif Numb(1) == 2
+               Str.Context = [Str.Context; 'C'];
+           end
+          Str.Direction = [Str.Direction; Numb(2)];
+          Str.Location = [Str.Location; Numb(3)];
+          Str.Association = [Str.Association; Numb(4)];
+          Str.Obj_ID = [Str.Obj_ID; Numb(5)];
+       %Object/choice On/Off, TrialEnd
+       elseif RawEventLog{i,2} == EventName(4) | RawEventLog{i,2} == EventName(5) | RawEventLog{i,2} == EventName(8) | RawEventLog{i,2} == EventName(9)
+           Str.(RawEventLog{i,2}) = [Str.(RawEventLog{i,2}); RawEventLog{i,1}];
+       % decision, duration
+       elseif RawEventLog{i,2} == EventName(6) | RawEventLog{i,2} == EventName(7)
+           Str.(RawEventLog{i,2}) = [Str.(RawEventLog{i,2}); RawEventLog{i,3}];
+       % Choice
+       elseif contains(char(RawEventLog{i,2}),"Choice"+lettersPattern)
+           Str.Choice = [Str.Choice; extractAfter(RawEventLog{i,2},"Choice")];
+       end
+        end
     end
 end
 
 
+EventTable = table ;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+          
 
 
 end
