139ba9d6fa736b34f8803d5a74fd60c6ad36585d
sorinjeong <127107005+sorinjeong@users.noreply.github.com>
2023-05-15 12:59:35 +0900

    Update OCATLogSampleParsingCode.m

--- OCATLogSampleParsingCode.m
+++ OCATLogSampleParsingCode.m
@@ -36,7 +36,7 @@ Eventlogfile wo/coordinate
                 ParsingPerTrial.TrialStart = [ParsingPerTrial.TrialStart; RawEventLog{i,2}];
 
                 if isempty(ParsingPerTrial.Trial) | ParsingPerTrial.Trial(end)==4; t=1;
-                elseif ParsingPerTrial.Trial(end)==1; t=2;elseif ParsingPerTrial.Trial(end)==2; t=3;elseif ParsingPerTrial.Trial(end)==3; t=4;end;
+                elseif ParsingPerTrial.Trial(end)==1; t=2;elseif ParsingPerTrial.Trial(end)==2; t=3;elseif ParsingPerTrial.Trial(end)==3; t=4;end
                 ParsingPerTrial.Trial = [ParsingPerTrial.Trial; t];
 
                 ParsingPerTrial.Lap = [ParsingPerTrial.Lap; LapNumNTime(find(ParsingPerTrial.TrialStart(end) > LapNumNTime(:,1),1,'last'),2)]
@@ -49,22 +49,35 @@ Eventlogfile wo/coordinate
                     % decision, duration
                 elseif EventName(i)=="Decision"
                     ParsingPerTrial.Decision = [ParsingPerTrial.Decision; RawEventLog{i,2}];
-                    ParsingPerTrial.Duration = [ParsingPerTrial.Decision; RawEventLog{i,4}];
+                    ParsingPerTrial.Duration = [ParsingPerTrial.Duration; RawEventLog{i,4}];
 
                     %Object|choice On|Off, TrialEnd
                 elseif ismember(EventName(i),VarName)
                     ParsingPerTrial.(EventName(i)) = [ParsingPerTrial.(EventName(i)); RawEventLog{i,2}];
 
                 end
-  
             end
+            %choice missing (choiceOn과 objOff 이벤트 개수와 choice 개수가 다를 경우 missing 삽입)
+            if length(ParsingPerTrial.ChoiceOn) == length(ParsingPerTrial.ObjOff) & length(ParsingPerTrial.ChoiceOn) ~= length(ParsingPerTrial.Choice)
+                ParsingPerTrial.Choice = [ParsingPerTrial.Choice; missing];end           
     end
-   
-    find(choice) %%%% choice on과 object off 사이에 chice가 없을 경우 missing으로 둘 것
-    
 
+    % 1,2 --> forest(F), city (C)로 변경
+    [r1,c1] = find(ParsingPerTrial.Context ==1);[r2,c2] = find(ParsingPerTrial.Context ==2); ParsingPerTrial.Context=string(ParsingPerTrial.Context)
+    ParsingPerTrial.Context(r1,1)="F"; ParsingPerTrial.Context(r2,1)="C"
 
 end
 
+% field 순서 맞추고 table화
+ParsingPerTrial= orderfields(ParsingPerTrial,VarName)
+LogTable=struct2table(ParsingPerTrial)
+
+
+%% save log data
+
+if ~isfolder (['D:\internship\MATLAB\23.05.09_O-CAT parsing\processed\' filenameo])
+mkdir(['D:\internship\MATLAB\23.05.09_O-CAT parsing\processed\' filenameo]); end
+
+save(['D:\internship\MATLAB\23.05.09_O-CAT parsing\processed\' filenameo '\' filenameo], "LogTable");
 
 
