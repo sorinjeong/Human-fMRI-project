1fbe1c65cfa986082b679ba264cbe7cbaabf15cc
sorinjeong <127107005+sorinjeong@users.noreply.github.com>
2023-06-23 16:51:03 +0900

    Add files via upload

--- addCorrect_effi.m
+++ addCorrect_effi.m
@@ -1,7 +1,7 @@
-% FileList = {'CL121121_1','CL121122_1','CL121128_1','CL121227_1','CL130107_1','CL130109_1','CL130114_2','CL130116_2',...
-%     'CL130121_2','CL130122_1','CL130130_1','CL130219_1','CL130220_1','CL130225_2','CL130226_1','CL130227_1'};
+FileList = {'CL121121_1','CL121122_1','CL121128_1','CL121227_1','CL130107_1','CL130109_1','CL130114_2','CL130116_2',...
+    'CL130121_2','CL130122_1','CL130130_1','CL130219_1','CL130220_1','CL130225_2','CL130226_1','CL130227_1'};
 
-FileList = {'CL121122_1'};
+% FileList = {'CL121122_1'};
 
 %%
 for fi = 1:numel(FileList)
@@ -60,14 +60,16 @@ end
        if ctr_ocpr == 1
            spm.Sess.U(4).ons(:,2) = ones(length(spm.Sess.U(4).ons),1);
            spm.Sess.U(5).ons(:,2) = JE_correct_ph2;
-           spm.Sess.U(6).ons(:,2) = JE_effidx;
-           spm.Sess.U(6).ons(:,3) = JE_correct_ph3;
+           
+           spm.Sess.U(6).ons(:,2) = JE_correct_ph3;
+           spm.Sess.U(6).ons(:,3) = JE_effidx;
            spm.Sess.U(6).ons(:,4) = Group;
        else
            spm.Sess.U(1).ons(:,2) = ones(length(spm.Sess.U(1).ons),1);
            spm.Sess.U(2).ons(:,2) = JE_correct_ph2;
-           spm.Sess.U(3).ons(:,2) = JE_effidx;
-           spm.Sess.U(3).ons(:,3) = JE_correct_ph3;        
+           
+           spm.Sess.U(3).ons(:,2) = JE_correct_ph3;  
+           spm.Sess.U(3).ons(:,3) = JE_effidx;
            spm.Sess.U(3).ons(:,4) = Group;    
        end
 end
