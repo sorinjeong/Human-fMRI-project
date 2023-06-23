0b9aec29d0ab18e7b361ea7f5f378253d27a25b0
sorinjeong <127107005+sorinjeong@users.noreply.github.com>
2023-06-23 17:38:20 +0900

    Add files via upload

--- addCorrect_effi.m
+++ addCorrect_effi.m
@@ -1,7 +1,7 @@
-FileList = {'CL121121_1','CL121122_1','CL121128_1','CL121227_1','CL130107_1','CL130109_1','CL130114_2','CL130116_2',...
-    'CL130121_2','CL130122_1','CL130130_1','CL130219_1','CL130220_1','CL130225_2','CL130226_1','CL130227_1'};
+% FileList = {'CL121121_1','CL121122_1','CL121128_1','CL121227_1','CL130107_1','CL130109_1','CL130114_2','CL130116_2',...
+%     'CL130121_2','CL130122_1','CL130130_1','CL130219_1','CL130220_1','CL130225_2','CL130226_1','CL130227_1'};
 
-% FileList = {'CL121122_1'};
+FileList = {'CL130227_1'};
 
 %%
 for fi = 1:numel(FileList)
@@ -27,15 +27,20 @@ for ctr_ocpr=1:2
     if ctr_ocpr == 1
        JE = ctr_Table;
        correct_phase1 = ones(length(spm.Sess.U(4).ons),1);
+       len = length(spm.Sess.U(4).ons);
 
     else
         JE = ocpr_Table;
        correct_phase1 = ones(length(spm.Sess.U(1).ons),1);
+       len = length(spm.Sess.U(1).ons);
     end
 
 %% correctness
-
+if len < 40
+    Table = readmatrix([datafolder '\' filenameo '\' JE],"Range",'A2:O40');
+else
 Table = readmatrix([datafolder '\' filenameo '\' JE],"Range",'A2:O41');
+end
 Table(ismissing(Table))=0;
 % JE_correct_ans = Table(:,2);
 JE_correct_ph2 = Table(:,5);
@@ -73,6 +78,7 @@ end
            spm.Sess.U(3).ons(:,4) = Group;    
        end
 end
+
 
 %% save
 if ~isfolder (['D:\internship\MATLAB\23.06.23_OPPA-corr_effi\' filenameo])
