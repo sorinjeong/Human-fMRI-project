4df21639181cd6b16bfafd09d9e5628e4e9eca31
sorinjeong <127107005+sorinjeong@users.noreply.github.com>
2023-07-11 21:20:06 +0900

    Add files via upload

--- ParticipantsScreening_230711.m
+++ ParticipantsScreening_230711.m
@@ -9,10 +9,10 @@ SubInfoFile = renamevars(SubInfoFile(2:19,[1 3 4 5]),["Var1","Var3","Var4","Var5
 %% session별 나누기
 Subs = unique(T.Session,"rows","sorted");
 
-SpliT=[];
+SpliT=[]; To=T; To((find(To.isTimeout==1)),:)=[];
 for i=1:length(Subs)
     varName = sprintf('SUB_%.15g',Subs(i));
-    SpliT.(varName) = T(find(T.Session(:) == Subs(i)),:);
+    SpliT.(varName) = To(find(To.Session(:) == Subs(i)),:);
 % end; clear("varName", "i");
 
 
@@ -26,5 +26,17 @@ Asso= sort([A(find(O==4,1)),4; A(find(O==5,1)),5; A(find(O==6,1)),6; A(find(O==7
 N=num2str(Asso(1:4,2)');SubInfoFile.Combi_FFCC=zeros(height(SubInfoFile),1);
 N(N ==' ')=[]; Combination = str2num(N);
 SubInfoFile.Combi_FFCC(find(SubInfoFile.Session == Subs(i)))=Combination;
+
+end; clear("varName", "i");
+
+%% Data Group (Pass/Fail , Correct/Overall)
+
+DataGroup = struct("PASS",[],"FAIL",[],"Correct",[],"Overall",[]);
+P=SubInfoFile.Session(find(SubInfoFile.PASS == 1));
+F=SubInfoFile.Session(find(SubInfoFile.PASS == 0));
+for n=1:length(P); DataGroup.PASS.(sprintf('SUB_%.15g', P(n))) = SpliT.(sprintf('SUB_%.15g', P(n))); end
+for n=1:length(F); DataGroup.FAIL.(sprintf('SUB_%.15g', F(n))) = SpliT.(sprintf('SUB_%.15g', F(n))); end
+
+
 
 
