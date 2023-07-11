be9cb55b340311b849686468148832cc47e280d6
sorinjeong <127107005+sorinjeong@users.noreply.github.com>
2023-07-11 20:41:36 +0900

    Add files via upload

--- ParticipantsScreening_230711.m
+++ ParticipantsScreening_230711.m
@@ -18,9 +18,13 @@ for i=1:length(Subs)
 
 %% Context-Object 조합
 
-C = SpliT.(varName).Context(find(SpliT.(varName).Correct == 1));
-O = SpliT.(varName).Object(find(SpliT.(varName).Correct == 1));
-A=[C O]
+C = SpliT.(varName).Context(find(SpliT.(varName).Association == 1));
+O = SpliT.(varName).Object(find(SpliT.(varName).Association == 1));
+A=[C O];
+Asso= sort([A(find(O==4,1)),4; A(find(O==5,1)),5; A(find(O==6,1)),6; A(find(O==7,1)),7]);
+
+N=num2str(Asso(1:4,2)');SubInfoFile.Combi_FFCC=zeros(height(SubInfoFile),1);
+N(N ==' ')=[]; Combination = str2num(N);
+SubInfoFile.Combi_FFCC(find(SubInfoFile.Session == Subs(i)))=Combination;
 
 
-A(find(O==4,1))
