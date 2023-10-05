234f320c47c4e6f75688bd46638decb140566e60
sorinjeong <127107005+sorinjeong@users.noreply.github.com>
2023-10-05 17:51:50 +0900

    Add files via upload

--- Dcm2BIDS_SR_v1.m
+++ Dcm2BIDS_SR_v1.m
@@ -5,18 +5,16 @@ preprocessed_path = 'D:\leelab\Human fMRI projects\0O-CAT\fMRI_ocat_data\1004pra
 % sbjNum = [85, 90, 96, 100];
 sbjNum = [102];
 sbjName = strcat('SUB',string(sbjNum));
-
-raw_sbj_dir = dir(fullfile(raw_data_path, sbjName, '**', 'series_*'));
-raw_sbj_path = raw_sbj_dir(1).folder;
-raw_sbj_folder = {raw_sbj_dir.name};
+raw_sbj_path = fullfile(raw_data_path, sbjName, '**', 'series_*');
+raw_sbj_folder = dir(raw_sbj_path);
 % raw_sbj_dir = fullfile(raw_sbj_path, {raw_sbj_folder.name});
 
 
 %% raw data folder: T1 / field-mag / field-phase / func
-raw_T1_idx = find(contains(raw_sbj_folder, "T1"));
-raw_fmap_mag_idx = find(contains(raw_sbj_folder, "AP"),1,"first");
-raw_fmap_phase_idx = find(contains(raw_sbj_folder, "AP"),1,"last");
-raw_func_idx = find(contains(raw_sbj_folder, "TASK5"));
+raw_T1_folder = raw_sbj_folder(find(contains({raw_sbj_folder.name}, "T1")));
+raw_fmap_mag_folder = raw_sbj_folder(find(contains({raw_sbj_folder.name}, "AP"),1,"first"));
+raw_fmap_phase_folder = raw_sbj_folder(find(contains({raw_sbj_folder.name}, "AP"),1,"last"));
+raw_func_folder = raw_sbj_folder(find(contains({raw_sbj_folder.name}, "TASK5")));
 
 
 
@@ -31,10 +29,10 @@ end
 
 
 %% converting T1 image
-sbj_T1_images = dir(fullfile(raw_sbj_folder(1).folder, raw_T1_folder.name)).name;
+sbj_T1_images = dir(fullfile(raw_T1_folder(1).folder, raw_T1_folder.name)).name;
 matlabbatch{1}.spm.util.import.dicom.data = sbj_T1_images;
 
 
-sbj_T1_images = 
+
 
 
