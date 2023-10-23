07eeffbdc5da9abacb61f81501f72ac9e8677d4d
sorinjeong <127107005+sorinjeong@users.noreply.github.com>
2023-10-23 18:59:35 +0900

    Add files via upload

--- Dcm2BIDS_SR_231023_sub1to18.m
+++ Dcm2BIDS_SR_231023_sub1to18.m
@@ -29,6 +29,15 @@ for sub = 1: length(sbjName)
 % raw data folder
 raw_sbj_path = fullfile(raw_data_path, sbjName(sub), 'study*', 'series_*');
 raw_sbj_folders = dir(raw_sbj_path);
+for i=1:length(raw_sbj_folders)
+fo = fullfile(raw_sbj_folders(i).folder, raw_sbj_folders(i).name);
+fi = dir(fullfile(fo, '*.IMA'));  % Find .IMA files
+if ~isempty(dir(fullfile(fo, 'sub8*.IMA'))) || ~isempty(dir(fullfile(fo, 'SUB9*.IMA'))) || ~isempty(dir(fullfile(fo, 'SUB10*.IMA')))
+for idx = 1:numel(fi)
+    movefile(fullfile(fo, fi(idx).name), fullfile(fo, strrep(fi(idx).name, string(sbjName(sub)), string(sbj))));
+end
+end
+end
 
 %% raw data folder: T1 / field-mag / field-phase / func
 raw_T1_dir = raw_sbj_folders(find(contains({raw_sbj_folders.name}, "T1")));
@@ -39,7 +48,7 @@ raw_func_dir = raw_sbj_folders(find(contains({raw_sbj_folders.name}, "TASK5")));
 %% make output directory
 output_dir = fullfile(preprocessed_path,sbj);
 if ~exist(output_dir,"dir")
-    mkdir(output_dir);
+    mkdir(output_dir);c
     mkdir(output_dir+'\anat');
     mkdir(output_dir+'\fmap\mag');
     mkdir(output_dir+'\fmap\phase');
@@ -50,11 +59,6 @@ end
 %% converting T1 image
 folder = fullfile(raw_T1_dir(1).folder, raw_T1_dir.name);
 files = dir(fullfile(folder, '*.IMA'));  % Find .IMA files
-if ~isempty(dir(fullfile(folder, 'SUB8*.IMA'))) || ~isempty(dir(fullfile(folder, 'SUB9*.IMA'))) || ~isempty(dir(fullfile(folder, 'SUB10*.IMA')))
-for idx = 1:numel(files)
-    movefile(fullfile(folder, files(idx).name), fullfile(folder, strrep(files(idx).name, string(sbjName(sub)), string(sbj))));
-end
-end
 sbj_T1_images = fullfile({files.folder}, {files.name});  % Create full paths for each file.
 
 matlabbatch{1}.spm.util.import.dicom.data = sbj_T1_images(:);
@@ -68,11 +72,6 @@ matlabbatch{1}.spm.util.import.dicom.convopts.icedims = 0;
 %% converting field map - magnitude
 folder = fullfile(raw_fmap_mag_dir(1).folder, raw_fmap_mag_dir.name);
 files = dir(fullfile(folder, '*.IMA'));  
-if ~isempty(dir(fullfile(folder, 'SUB8*.IMA'))) || ~isempty(dir(fullfile(folder, 'SUB9*.IMA'))) || ~isempty(dir(fullfile(folder, 'SUB10*.IMA'))) 
-for idx = 1:numel(files)
-    movefile(fullfile(folder, files(idx).name), fullfile(folder, strrep(files(idx).name, string(sbjName(sub)), string(sbj))));
-end
-end
 sbj_fmap_mag = fullfile({files.folder}, {files.name});
 
 matlabbatch{2}.spm.util.import.dicom.data = sbj_fmap_mag(:);
@@ -86,11 +85,6 @@ matlabbatch{2}.spm.util.import.dicom.convopts.icedims = 0;
 %% converting field map - phase
 folder = fullfile(raw_fmap_phase_dir(1).folder, raw_fmap_phase_dir.name);
 files = dir(fullfile(folder, '*.IMA'));  
-if ~isempty(dir(fullfile(folder, 'SUB8*.IMA'))) || ~isempty(dir(fullfile(folder, 'SUB9*.IMA'))) || ~isempty(dir(fullfile(folder, 'SUB10*.IMA')))
-for idx = 1:numel(files)
-    movefile(fullfile(folder, files(idx).name), fullfile(folder, strrep(files(idx).name, string(sbjName(sub)), string(sbj))));
-end
-end
 sbj_fmap_phase = fullfile({files.folder}, {files.name});
 
 matlabbatch{3}.spm.util.import.dicom.data = sbj_fmap_phase(:);
@@ -104,11 +98,6 @@ matlabbatch{3}.spm.util.import.dicom.convopts.icedims = 0;
 %% converting MR image
 folder = fullfile(raw_func_dir(1).folder, raw_func_dir.name);
 files = dir(fullfile(folder, '*.IMA'));  
-if ~isempty(dir(fullfile(folder, 'SUB8*.IMA'))) || ~isempty(dir(fullfile(folder, 'SUB9*.IMA')))|| ~isempty(dir(fullfile(folder, 'SUB10*.IMA')))
-for idx = 1:numel(files)
-    movefile(fullfile(folder, files(idx).name), fullfile(folder, strrep(files(idx).name, string(sbjName(sub)), string(sbj))));
-end
-end
 sbj_MR_images = fullfile({files.folder}, {files.name});
 
 matlabbatch{4}.spm.util.import.dicom.data = sbj_MR_images(:);
