cdfe81fb84c9ca533a3fd13eafd84026f3a38893
sorinjeong <127107005+sorinjeong@users.noreply.github.com>
2023-11-25 18:44:29 +0900

    Add files via upload

---
+++ code_fmri_dicm2nii.m
@@ -0,0 +1,71 @@
+clear all; clc;
+addpath(genpath('\\147.47.203.154\LeeStorage1\E-Phys Analysis\fMRI_ocat\OCAT_DIR'));
+
+%%
+file_path_in = '../data/data_fmri_raw_Num';
+file_path_out = '../data/data_fmri_bids'; 
+
+%% set subject's root directory
+n_sbj = 31;
+
+for sbj_i = 1: n_sbj
+    c_sbj = num2str(sbj_i, '%02.f');
+    
+    %% path_in == raw data folder: T1 / field-mag / field-phase / func
+    raw_sbj_folders = dir(fullfile(file_path_in, c_sbj, 'study*', 'series_*'));
+    path_in = {};
+    path_in{end+1} = raw_sbj_folders(find(contains({raw_sbj_folders.name}, "T1"),1,"last")); % T1 image
+    path_in{end+1} = raw_sbj_folders(find(contains({raw_sbj_folders.name}, "TASK"),1,"last")); % MR image
+    path_in{end+1} = raw_sbj_folders(find(contains({raw_sbj_folders.name}, "AP"),1,"first")); % field map - magnitude
+    path_in{end+1} = raw_sbj_folders(find(contains({raw_sbj_folders.name}, "AP"),1,"last")); % field map - phase
+
+
+%% converting image to nifti
+    for pth = 1:length(path_in)
+    dicomDir = fullfile(path_in{1,pth}.folder, path_in{1,pth}.name);
+    outputDir = file_path_out;  
+    
+   
+    dicm2nii_OCAT(dicomDir, outputDir, 'bids');
+    
+    end
+end
+
+
+%     %% arrayfun / cellfun 사용!
+%     for pth = 1:length(path_in)
+%     file_list = dir(fullfile(path_in{1,pth}.folder, path_in{1,pth}.name));
+%     file_name = arrayfun(@(x) x.name, file_list, 'uni',0);
+%     flag_dicom = cellfun(@(x) contains(x, '.IMA'), file_name);
+% 
+%     file_list = arrayfun(@(x) fullfile(x.folder, x.name), file_list(flag_dicom), 'uni', 0);
+%     
+
+%% JSON file
+% Make 'dataset_description.json' as structure
+dataset_description = struct(...
+    'Name', 'Object-Context Association Task','TaskName', 'OCAT',...
+    'BIDSVersion', '1.0.2',...
+    'License', 'CC0',...
+    'Authors', {{'Sorin Jeong', 'Zyeon Kim'}},...
+    'Acknowledgements', 'We extend our thanks to Ji-Sun Kim and Jae-Min Seol for their assistance in data acquisition and analysis. We also want to express our gratitude to all the participants of this study.',...
+    'HowToAcknowledge', 'Please cite this paper.');
+
+% save as JSON file
+jsonStr = jsonencode(dataset_description);
+fid = fopen([file_path_out '\dataset_description.json'], 'w');
+if fid == -1, error('Cannot create JSON file'); end
+fwrite(fid, jsonStr, 'char');
+fclose(fid);
+
+% ,...
+%     'Funding', {{'Grant 1', 'Grant 2'}},...
+%     'ReferencesAndLinks', {{'Link 1', 'Link 2'}},...
+%     'DatasetDOI', '10.0.2.3/abcd'...
+
+
+% 'Participant_id' 열에서 첫 번째 행을 제외하고 "sub-" 추가
+cd(outputDir)
+T = readtable('participants.tsv', 'FileType', 'text', 'Delimiter', '\t');
+T.participant_id(2:end) = strcat('sub-', string(T.participant_id(2:end)));
+writetable(T, 'participants.tsv', 'FileType', 'text', 'Delimiter', '\t');
