a8f5748bdb56698eeff7893e772433bfc00e5045
sorinjeong <127107005+sorinjeong@users.noreply.github.com>
2024-01-19 21:07:37 +0900

    Add files via upload

---
+++ SR_glm_1st_SPM_batch_job_240113_ver3.m
@@ -0,0 +1,110 @@
+clear; clc; close all;
+
+
+%% Initialise SPM defaults
+spm('defaults', 'FMRI');
+spm_jobman('initcfg');
+
+%-----------------------------------------------------------------------
+% Job saved on 13-Jan-2024 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+path_in = 'D:\fMRI\OCAT_DIR\data\data_fmri_bids\derivatives'; % fmriprep's output file (before smoothing)
+path_out = 'D:\fMRI\OCAT_DIR\data\lev-1st_ver4'; % save betas this folder
+addpath(path_in);
+condition_path = 'D:\fMRI\OCAT_DIR\data\data_for_glm';addpath(condition_path);
+load('regressor_all_all'); 
+
+
+%% 
+for i = 1:length(sbj_id_list)
+    c_sbj = sprintf('sub-%.2d', sbj_id_list(i));
+    
+    sbj_dir = fullfile(path_in, c_sbj);
+    current_beta_out = fullfile(path_out,c_sbj);mkdir(current_beta_out);
+%     current_file_in = fullfile(file_in.folder,file_in.name);
+
+        % Here lines differ for 3D vs. 4D %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % This is for 4D multiple files
+    % get herder information to read a 4D file
+%     scan_files=[];
+%     tmpHdr = spm_vol( current_file_in );
+%     f_list_length = size(tmpHdr, 1);  % number of 3d volumes
+%     for jx = 1:f_list_length
+%         scan_files{jx,1} = [current_file_in ',' num2str(1) ] ; % add numbers in the end
+%         % End of difference for 3D vs. 4D %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%     end
+%     
+%% smoothing
+
+%     file_in = dir(fullfile(sbj_dir,'func',[c_sbj '*preproc_bold.nii']));
+%     current_file_in = cellstr(fullfile(file_in.folder,file_in.name));
+% 
+% 
+% matlabbatch{1}.spm.spatial.smooth.data = {current_file_in};
+% matlabbatch{1}.spm.spatial.smooth.fwhm = [6 6 6];
+% matlabbatch{1}.spm.spatial.smooth.dtype = 0;
+% matlabbatch{1}.spm.spatial.smooth.im = 0;
+% matlabbatch{1}.spm.spatial.smooth.prefix = 's';
+% 
+% 
+
+%% Version : smoothing X
+
+    file_in = dir(fullfile(sbj_dir,'func',['s' c_sbj '*preproc_bold.nii']));
+    current_file_in = cellstr(fullfile(file_in.folder,file_in.name));
+ 
+matlabbatch = [];
+matlabbatch{1}.spm.stats.fmri_spec.dir = {current_beta_out};
+matlabbatch{1}.spm.stats.fmri_spec.timing.units = 'secs';
+matlabbatch{1}.spm.stats.fmri_spec.timing.RT = 2;
+matlabbatch{1}.spm.stats.fmri_spec.timing.fmri_t = 16;
+matlabbatch{1}.spm.stats.fmri_spec.timing.fmri_t0 = 8;
+
+matlabbatch{1}.spm.stats.fmri_spec.sess.scans = current_file_in;
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {}, 'orth', {});
+matlabbatch{1}.spm.stats.fmri_spec.sess.multi = {fullfile(condition_path,[c_sbj, '_multiple_conditions.mat'])};
+matlabbatch{1}.spm.stats.fmri_spec.sess.regress = struct('name', {}, 'val', {});
+matlabbatch{1}.spm.stats.fmri_spec.sess.multi_reg = {fullfile(condition_path,[c_sbj, '_movereg.mat'])};
+matlabbatch{1}.spm.stats.fmri_spec.sess.hpf = 128;
+
+matlabbatch{1}.spm.stats.fmri_spec.fact = struct('name', {}, 'levels', {});
+matlabbatch{1}.spm.stats.fmri_spec.bases.hrf.derivs = [0 0];
+matlabbatch{1}.spm.stats.fmri_spec.volt = 1; %Model Interactions (Volterra) / 1==no interaction , 2==interaction 
+matlabbatch{1}.spm.stats.fmri_spec.global = 'None';
+matlabbatch{1}.spm.stats.fmri_spec.mthresh = 0.8;
+matlabbatch{1}.spm.stats.fmri_spec.mask = {''};
+matlabbatch{1}.spm.stats.fmri_spec.cvi = 'AR(1)';
+spm_jobman('run', matlabbatch)
+
+%%
+matlabbatch = [];
+matlabbatch{1}.spm.stats.fmri_est.spmmat = { fullfile(current_beta_out, 'SPM.mat') };
+matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
+spm_jobman('run', matlabbatch)
+
+%%
+matlabbatch = [];
+matlabbatch{1}.spm.stats.con.spmmat  = { fullfile(current_beta_out, 'SPM.mat') };
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.name = 'corr-incorr';
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.weights = [0 1 -1 1 -1 0 0 0 0 0];
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.name = 'corr_YES-corr_NO';
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.weights = [0 1 0 -1 0];
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.name = 'corr_NO-corr_YES';
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.weights = [0 -1 0 1 0];
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.delete = 1;
+
+spm_jobman('run', matlabbatch)
+
+end
+
+
+%%%%% 2:37am : 지금 계속 #31번피험자 (28번째) 가 fMRI model specification 실패중임. 일단
+%%%%% 제외하고 2nd 진행
+
+%%%%% 2024/01/19 8pm : #31번 smoothing 새로 하고 (전에도 smoothing 다시 했었지만 그 땐
+%%%%% 안됐었는데..) 한 블럭 한블럭씩 하니까 됨. smoothing 한번 돌리고 specification 돌리고 이런식으로.
---
+++ glm_2nd_SPM_240119_ver_click_job_job.m
@@ -0,0 +1,56 @@
+clear; clc; close all;
+
+
+%% Initialise SPM defaults
+spm('defaults', 'FMRI');
+spm_jobman('initcfg');
+
+%-----------------------------------------------------------------------
+% Job saved on 19-Jan-2024 20:30:17 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+%%
+path_in = 'D:\fMRI\OCAT_DIR\data\lev-1st_ver3'; % 1st level GLM - output file 
+path_out = 'D:\fMRI\OCAT_DIR\data\lev-2nd-ver4'; % save betas this folder
+addpath(path_in);
+condition_path = 'D:\fMRI\OCAT_DIR\data\data_for_glm';addpath(condition_path);
+load('regressor_all_all'); 
+
+%% set conditions
+
+cond_name = {'corr-incorr','corr_YES-corr_NO', 'corr_NO-corr_YES'};
+cond_num = {'con_0001','con_0002','con_0003'};
+
+for c=1:3
+    fprintf('%s%s\n','current condition : ',cond_name{c});
+    
+%% set path
+ current_path_out = fullfile(path_out,cond_name{c}); mkdir(current_path_out);
+%% set files
+current_scans = char(arrayfun(@(i) [path_in,'\sub-', sprintf('%02d', i), '\',cond_num{c},'.nii,1'], sbj_id_list, 'UniformOutput', false));
+
+%% 
+matlabbatch=[];
+matlabbatch{1}.spm.stats.factorial_design.dir = cellstr(current_path_out);
+matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = cellstr(string(current_scans));
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
+matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'corr-incorr';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = 1;
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.delete = 1;
+
+spm_jobman('run', matlabbatch)
+
+end
